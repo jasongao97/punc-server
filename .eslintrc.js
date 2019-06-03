5ffd178743370e6ec8dcc99235e54fc8651bf6ee
jasongao97 <jasongao97@gmail.com>
2019-06-03 21:58:59 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
     "extends": "airbnb-base",
     "rules": {
-        // "linebreak-style": "off",
+        "linebreak-style": "off",
         // "comma-dangle": ["error", "never"],
         // "operator-linebreak": ["error", "none"]
     }
